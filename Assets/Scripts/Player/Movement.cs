860209cd96d1d3ce195bdae453108376d4547c80
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2021-11-07 00:54:50 +0200

    Added running behaviour

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float walkSpeed = 6f;
+    [SerializeField] private float runningSpeed = 13f;
     private CharacterController controller;
     private float minMagnitude = 0.01f;
+    private float speed = 0;
 
 
     private void Awake()
@@ -24,6 +26,10 @@ public class Movement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift))
+            speed = runningSpeed;
+        else
+            speed = walkSpeed;
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         controller.SimpleMove(movementDirection * speed);
