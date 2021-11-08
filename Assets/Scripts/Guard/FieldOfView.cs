84e16a94ad6366a6bc9463ad3259447e7dbca43c
Cristian <cri.ghiorzi@gmail.com>
2021-11-08 16:54:43 +0100

    Guard's Field of View Visualization
    added an in game visualization of Guard's FoV

--- Assets/Scripts/Guard/FieldOfView.cs
+++ Assets/Scripts/Guard/FieldOfView.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
+    private Mesh _viewMesh;
+
     [Header("FOW Settings")]
     public float ViewRadius;
     [Range(0,360)]
     public float ViewAngle;
+    
+    public float MeshResolution; // raycast per degree
+    public MeshFilter ViewMeshFilter;
 
     public LayerMask targetMask;
     public LayerMask obstacleMask;
@@ -22,11 +27,20 @@ public class FieldOfView : MonoBehaviour
     {
         PlayerInRange = false;
         PlayerPosition = Vector3.zero;
+
+        _viewMesh = new Mesh();
+        _viewMesh.name = "View Mesh";
+        ViewMeshFilter.mesh = _viewMesh;
     }
 
     private void Start()
     {
         StartCoroutine(FindTargetsWithDelay(0.2f));
+    }
+
+    private void LateUpdate()
+    {
+        DrawFieldOfView();
     }
 
     private IEnumerator FindTargetsWithDelay(float delay)
@@ -65,6 +79,58 @@ public class FieldOfView : MonoBehaviour
         }
     }
 
+    private void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(ViewAngle * MeshResolution); // rays count
+        float stepAngleSize = ViewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+
+        for(int i=0; i<=stepCount; ++i)
+        {
+            float angle = transform.eulerAngles.y - ViewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoints.Add(newViewCast.Point);
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero; // mesh is child of the GO - so the origin of the mesh's triangles is .zero and not transform.position
+        for (int i = 0; i < vertexCount - 2; ++i)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        _viewMesh.Clear();
+        _viewMesh.vertices = vertices;
+        _viewMesh.triangles = triangles;
+        _viewMesh.RecalculateNormals();
+
+    }
+
+    private ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, dir, out hit, ViewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * ViewRadius, ViewRadius, globalAngle);
+        }
+    }
+
     // Direction of lines of the view angle of fow - angleIsGlobal = false -> angle rotate with character
     public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
@@ -74,5 +140,21 @@ public class FieldOfView : MonoBehaviour
         }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
-  
+
+    public struct ViewCastInfo
+    {
+        public bool Hit;
+        public Vector3 Point;
+        public float Dist;
+        public float Angle;
+
+        public ViewCastInfo(bool hit, Vector3 point, float dist, float angle)
+        {
+            Hit = hit;
+            Point = point;
+            Dist = dist;
+            Angle = angle;
+        }
+    }
+
 }
