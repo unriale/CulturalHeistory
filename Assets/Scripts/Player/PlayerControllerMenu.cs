5f86b1645a658edd92a87004a9b12c670fba54b6
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2021-11-18 15:23:35 +0200

    Minor menu improvements

--- Assets/Scripts/Player/PlayerControllerMenu.cs
+++ Assets/Scripts/Player/PlayerControllerMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,18 @@ using UnityEngine;
 public class PlayerControllerMenu : Movement
 {
     private float horizontalMoveVal = 1;
+    private float buttonPos;
+    private float position1 = 0.3f;
+    private float position2 = 2f;
+    [SerializeField] RectTransform options;
+    [SerializeField] RectTransform start;
 
     private void OnTriggerEnter(Collider other)
     {
         horizontalMoveVal = horizontalMoveVal == 1 ? -1 : 1;
+        buttonPos = buttonPos == position1 ? position2 : position1;
+        options.position = new Vector3(options.position.x, options.position.y, buttonPos);
+        start.position = new Vector3(start.position.x, start.position.y, buttonPos);
     }
 
     protected override void Move()
