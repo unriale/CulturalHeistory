0d83fb780b5c5c12cc3541c999a55e2bbbce204b
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2021-11-21 11:59:55 +0200

    China models

---
+++ Assets/Models/china/Gossip_Altar/Prefab/AutoRot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRot : MonoBehaviour {
+
+    public float speed = 30f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, speed, 0)*Time.deltaTime);
+	}
+}
